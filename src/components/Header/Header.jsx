433dd5007d654bda75bf6156153f01504322919a
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-22 11:26:03 +0600

    add active nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className="nav-container">
             <nav className="text-2xl font-medium my-10">
                 {/* reload the page */}
                 {/* <a className="m-6" href="/">Home</a>
@@ -11,10 +12,16 @@ const Header = () => {
                 <a className="m-6" href="/contact">Contact Us</a> */}
                 
                 {/* thats why use Link */}
-                <Link className="m-10" to="/">Home</Link>
+                {/* <Link className="m-10" to="/">Home</Link>
                 <Link className="m-10" to="/users">Users</Link>
                 <Link className="m-10" to="/about">About</Link>
-                <Link className="m-10" to="/contact">Contact Us</Link>
+                <Link className="m-10" to="/contact">Contact Us</Link> */}
+
+                {/* active navlink  */}
+                <NavLink className="m-10" to="/">Home</NavLink>
+                <NavLink className="m-10" to="/users">Users</NavLink>
+                <NavLink className="m-10" to="/about">About</NavLink>
+                <NavLink className="m-10" to="/contact">Contact Us</NavLink>
             </nav>
         </div>
     );
